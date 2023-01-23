59f7e5041706e40aee1aaeb0272f2218605d631c
amesjx <jxing25@moundsparkacademy.org>
2023-01-23 16:53:56 -0600

    Added imports

--- ChargedUp/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ ChargedUp/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,15 +4,20 @@
 
 package frc.robot.subsystems;
 
+import javax.script.Bindings;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase 
 {
+
+
   /** Creates a new IntakeSubsystem. */
   // creating motors
   private final CANSparkMax intakeMotorR = new CANSparkMax(IntakeConstants.INTAKE_MOTOR_R_ID, MotorType.kBrushless);
