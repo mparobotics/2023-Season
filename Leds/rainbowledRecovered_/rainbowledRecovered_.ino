f14d50f84a13ce0add0035624e4fd035a4b45512
AlexHuie <ahuie26@moundsparkacademy.org>
2023-02-27 18:07:56 -0600

    added more led patterns

--- Leds/rainbowledRecovered_/rainbowledRecovered_.ino
+++ Leds/rainbowledRecovered_/rainbowledRecovered_.ino
@@ -2,9 +2,14 @@
 #include "FastLED.h"
 #define NUM_LEDS 80
 CRGB leds[NUM_LEDS];
-int color = 1;
+//0: Blue pattern
+//1: Red pattern
+//2: purple/yellow
+//3:  blue/white
+//4: rainbow
+int color = 4;
 void setup() { 
-  pinMode(11, OUTPUT);
+  
   FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS); 
   for(int i = 0; i < NUM_LEDS; i++) {
       
@@ -19,7 +24,7 @@ void setup() {
 }
 
 void loop() {
-    digitalWrite(11, HIGH);
+   
   
     //int colors[6] = {CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple};
      CRGB hi[7] = {CRGB::Red, CRGB::Orange, CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Purple, CRGB::Red};
@@ -37,7 +42,7 @@ void loop() {
       else if(color == 1){
         leds[i] = CRGB(127 + ((NUM_LEDS-i-j) % length)*128/length,0,0);  
       }
-      else{
+      else if(color == 2){
         CRGB purple = CRGB(50,0,255);
         CRGB yellow = CRGB(255,255,0);
         int border1 =  j % NUM_LEDS;
@@ -50,7 +55,28 @@ void loop() {
           leds[i] = yellow;
         }
       }
-         
+      else if (color == 3){
+        CRGB white = CRGB(255,255,255);
+        CRGB blue = CRGB(0,127,255);
+        int border1 =  j % NUM_LEDS;
+        int border2 = (j + NUM_LEDS/2) % NUM_LEDS;
+        
+        if( (i < border1|| i > border2) && (border2 > border1) || (i > border2 && i < border1 && border2 < border1)){
+          
+          
+          
+          leds[i] = blue;
+        }
+        
+        else{
+          leds[i] = white;
+        }
+       
+        
+      }
+      else if (color == 4){
+        leds[i] = makeColor(float(i+j)/NUM_LEDS * 360);
+      }
     } 
 
     FastLED.show();
@@ -58,21 +84,21 @@ void loop() {
   } 
 
 }
-
-CRGB Scroll(int pos) {
-  CRGB color (0,0,0);
-  if(pos < 85) {
+CRGB makeColor(int pos) {
+  CRGB color (255,255,255);
+  pos %= 360;
+  if(pos < 120) {
     color.g = 0;
-    color.r = ((float)pos / 85.0f) * 255.0f;
-    color.b = 256 - color.r;
-  } else if(pos < 170) {
-    color.g = ((float)(pos - 85) / 85.0f) * 255.0f;
+    color.r = (float(pos)/ 120.0f) * 255.0f;
+    color.b = 255 - color.r;
+  } else if(pos < 240) {
+    color.g = (float(pos - 120) / 120.0f) * 255.0f;
     color.r = 256 - color.g;
     color.b = 0;
-  } else if(pos < 400) {
-    color.b = ((float)(pos - 170) / 85.0f) * 255.0f;
+  } else {
+    color.b = (float(pos - 240) / 120.0f) * 255.0f;
     color.g = 256 - color.b;
-    color.r = 1;
+    color.r = 0;
   }
   return color;
 }
